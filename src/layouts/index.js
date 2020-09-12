598dca13921f1f420fa12cbb62ebb62ae46f05de
Allen Joseph <allenxavier45@gmail.com>
2020-09-12 18:21:17 +0530

    Update index.js

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -35,7 +35,6 @@ const Layout = ({ children, data }) => (
               <ul className="tabs">
                   <li><Link to="/" exact={true} activeClassName="active">Instructions</Link></li>
                   <li><Link to="/submissions/" exact={true} activeClassName="active">Submissions</Link></li>
-                  <li><Link to="/cheatsheet/" exact={true} activeClassName="active">CheatSheet</Link></li>
               </ul>
           </div>
           <div className="content">
